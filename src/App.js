502ef73cf00fefadf84b39426bd5a5880286f682
pushpendravidyaintern <pushpendra.s@vidyainternahub.com>
2025-01-30 15:04:53 +0530

    80% completed nav & hero section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App font-bs-font w-screen flex flex-col items-center">
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
