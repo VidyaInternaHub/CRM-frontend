24de8378eb3bd718e59b26d5d4c84a504cf19e1d
pushpendravidyaintern <pushpendra.s@vidyainternahub.com>
2025-02-12 12:59:09 +0530

    fixing alignment of Navbar Resources dropdown of mobile view

--- src/components/navComponents/NavResources.jsx
+++ src/components/navComponents/NavResources.jsx
@@ -14,7 +14,7 @@ const NavResources = () => {
             icon: (
                 <svg width="18" height="22" viewBox="0 0 18 22" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18 16.203c0 .343-.29.621-.646.621H4.038c-.74 0-1.345.583-1.347 1.294 0 .712.606 1.294 1.347 1.294h13.316c.357 0 .646.278.646.621v1.346c0 .343-.29.621-.646.621H4.038C1.817 22 0 20.253 0 18.118V3.882C0 1.747 1.817 0 4.038 0h13.316c.357 0 .646.278.646.621v15.582z"></path></svg>
             ),
-            content: ["Webinars", "Helpdisk", "Guides & API Documentation"]
+            content: ["Webinars", "Helpdesk", "Guides & API Documentation"]
         },
         {
             id: 2,
@@ -60,8 +60,8 @@ const NavResources = () => {
                                     <ul className='flex flex-col gap-y-4 justify-center items-start'>
                                         {tab.content.map((item, index) => (
                                             <div className='flex gap-2 justify-start items-center'>
-                                                {(tab.id === 1 && index != 0 || (tab.id === 2 && index === 0)) && <FaExternalLinkAlt fill='#409eef' />}
-                                                <li key={index} className='hover:text-[#409eef] ml-5 text-lg'><a href="">{item}</a></li>
+                                                {((tab.id === 1 && index !== 0) || (tab.id === 2 && index === 0)) && <FaExternalLinkAlt fill='#409eef' />}
+                                                <li key={index} className='hover:text-[#409eef] ml-5 text-lg'><a href="_">{item}</a></li>
                                             </div>
                                         ))}
                                     </ul>
@@ -75,14 +75,14 @@ const NavResources = () => {
 
             {/* Small devices */}
 
-            <section className='lg:hidden block absolute top-[100%] left-0 right-0 bg-white rounded-b-2xl shadow-lg p-6'>
+            <section className='lg:hidden block bg-white'>
                 <div className='w-full flex flex-col'>
                     {
                         tabs.map((tab) => (
                             <div className='flex flex-col'
                                 key={tab.id}
                             >
-                                <div key={tab.id} className={`relative flex justify-between items-center p-2 w-full rounded-full  ${activeTab === tab.id && 'bg-slate-100'} border-gray-100 border-spacing-1`}
+                                <div key={tab.id} className={`relative flex justify-between items-center py-2 pr-1 w-full rounded-full  ${activeTab === tab.id && 'bg-slate-100'} border-gray-100 border-spacing-1`}
                                     onClick={() => handleContent(tab.id)}
                                 >
                                     <div className='w-full flex justify-start  gap-x-2  text-gray-400  hover:text-[#409eef] group'>
@@ -102,8 +102,8 @@ const NavResources = () => {
                                         <ul className='flex flex-col gap-y-2 justify-center items-start w-full list-disc text-gray-700'>
                                             {tabs[activeTab - 1].content.map((item, index) => (
                                                 <div className='flex flex-col gap-1 w-full ml-4'>
-                                                    <li key={index} className='hover:text-[#409eef] ml-5 text-[1rem]'><a href="">{item}</a></li>
-                                                    <hr className='h-[1px] bg-gray-300 w-full' />
+                                                    <li key={index} className='hover:text-[#409eef] text-[1rem]'><a href="_">{item}</a></li>
+                                                    {/* <hr className='h-[1px] bg-gray-300 w-full' /> */}
                                                 </div>
 
                                             ))}
